ad55f485240b4e01ed3eef706c210147c33b424a
Phpesher <superduperproger@gmail.com>
2020-07-15 10:50:48 +0300

    Rewrite all clent on ts

---
+++ client/src/Components/Neon/Forms/NeonInput.tsx
@@ -0,0 +1,43 @@
+import * as React from "react"
+import {Form} from "react-bootstrap"
+
+const styles: object = {
+  fontFamily: 'Merienda One, cursive',
+  color: '#c6e2ff',
+  border: 'none',
+  fontSize: '2rem',
+  textAlign: 'center',
+  lineHeight: '1',
+  backgroundColor: 'rgba(0, 0, 0, 0)',
+  animation: 'neon .5s ease-in-out infinite alternate'
+}
+
+interface INeonInput {
+  type: string;
+  placeholder: string;
+  onClick: any;
+  onChange: any;
+  name: string;
+}
+
+const NeonInput: React.SFC<INeonInput> = ({
+    type,
+    placeholder,
+    onClick,
+    onChange,
+    name
+  }) => {
+  return (
+    <Form.Control
+      type={type}
+      style={styles}
+      placeholder={placeholder}
+      onClick={onClick}
+      onChange={onChange}
+      required
+      name={name}
+    />
+  )
+}
+
+export default NeonInput
---
+++ client/src/Components/Neon/NeonText.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface INeonText {
+  onClick?: any;
+  neonText: string;
+  style?: object;
+}
+
+const styles: object = {
+  fontSize: '2rem',
+  textAlign: 'center',
+  lineHeight: '1',
+  color: '#c6e2ff',
+  animation: 'neon .2s ease-in-out infinite alternate'
+}
+
+const NeonText: React.SFC<INeonText> = ({onClick, neonText}) => {
+  return (
+    <div style={styles} className="sign__word" onClick={onClick}>
+      {neonText}
+    </div>
+  )
+}
+
+export default NeonText
